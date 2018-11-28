c0436988dba61bacc357becc2c902b999ad502c4
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-11-28 03:03:17 -0500

    Implemented GraphQL List

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,8 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // 1. Define Types
@@ -18,13 +19,19 @@ const {
 let books = [
   { id: 1, name: "Harry Porter", genre: "Fantasy", authorId: 1 },
   { id: 2, name: "Lord Of The Rings", genre: "Fantasy", authorId: 2 },
-  { id: 3, name: "Don", genre: "Bollywood", authorId: 3 }
+  { id: 3, name: "Don", genre: "Bollywood", authorId: 3 },
+  { id: 4, name: "Harry Porter 2", genre: "Fantasy", authorId: 1 },
+  { id: 4, name: "Harry Porter 3", genre: "Fantasy", authorId: 1 },
+  { id: 5, name: "Don 2", genre: "Bollywood", authorId: 3 },
+  { id: 6, name: "The Great Gatsby", genre: "Fiction", authorId: 4 },
+  { id: 7, name: "The Curious Case of Benjamin Button", genre: "Fiction", authorId: 4 },
 ];
 
 let authors = [
   { id: 1, name: "J.K.Rowling", age: 53 },
   { id: 2, name: "J.R.R", age: 87 },
-  { id: 3, name: "Sharukh Khan", age: 53 }
+  { id: 3, name: "Sharukh Khan", age: 53 },
+  { id: 4, name: "Scott Fitzgerald", age: 44 }
 ];
 
 const BookType = new GraphQLObjectType({
@@ -36,7 +43,6 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        console.log({ parent })
         return _.find(authors, { id: parent.authorId });
       }
     }
@@ -48,7 +54,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLInt },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books : {
+        type: new GraphQLList(BookType),
+        resolve(parent,args){
+            return _.filter(books, {authorId: parent.id})
+        }
+    }
   })
 });
 
