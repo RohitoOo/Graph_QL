9a50da55cb3d7609a20c65be208cdab77012cb56
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-11-27 21:20:25 -0500

    Added Root Query, Implemented Loadash and Setup graphiql: true

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql')
-
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 const app = express();
 
+// Middle Ware ( One SuperCharged Endpoint To Rule Them All )
 
-// Middle Ware 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    // Options
+    // Schema
+    schema,
+    // Used To Test GraphQL API - Similar To PostMan
+    graphiql: true
+  })
+);
 
-app.use('/graphql', graphqlHTTP({
-    
-}))
-
-
-const port = 3000
-app.listen( port , () => {
-    console.log("We Are Live On Port ", port)
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log("We Are Live On Port ", port);
+});
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,42 +1,46 @@
-const graphql = require('graphql')
+const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
-const BookType = new GraphQLObjectType ({
-     name: "Books",
-     fields: () => ({
-         id: {type: GraphQLString},
-         name: {type: GraphQLString},
-         genre: {type: GraphQLString}
-     })
-})
+// 1. Define Types
+// 2. Define Relationships
+// 3. Define RootQuery
 
-// Where the User Can Jump in From the Front End To Grab Data 
+// Dummy Data
 
-const RootQuery = new GraphQLObjectType ({
-    name: "RootQueryType",
-    fields: {
-        book: {
-            type: BookType,
-            args: { id: GraphQLString }
-        }
+let books = [
+  { id: "1", name: "Harry Porter", genre: "Fantasy" },
+  { id: "2", name: "Lord Of The Rings", genre: "Fantasy" },
+  { id: "3", name: "Don", genre: "Bollywood" }
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Books",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+// Where the User Can Jump in From the Front End To Grab Data
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } }, // Required To Query --> book(id:"123"){ name genre }
+      resolve(parent, args) {
+        // code to get data from Database or any other Source
+
+        return _.find(books, { id: args.id });
+      }
     }
-})
+  }
+});
 
-// Where the User Can Jump in From the Front End To Grab Data 
-
-const RootQueryTest = new GraphQLObjectType ({
-    name: "RootQueryType",
-    fields: {
-        book: {
-            type: BookType,
-            args: { id: GraphQLString }
-        }
-    }
-})
-
-function masterBranchFunction(){
-    console.log("testing from Master Branch")
-function testGit(){
-    console.log("testing 123")
-}
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
